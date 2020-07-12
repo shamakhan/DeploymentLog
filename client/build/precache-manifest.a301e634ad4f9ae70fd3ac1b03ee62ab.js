28fd5b13a15044c6100e5696b8a0ff0cae53453f
Shama Khan <shama.k@media.net>
2020-07-12 23:14:39 +0530

    Client app built and pushed to repo

---
+++ client/build/precache-manifest.a301e634ad4f9ae70fd3ac1b03ee62ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66c07ccc3c01bfa6b69f85599302604",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a3d2cecc64035ccf4f8",
+    "url": "/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "d6595b185af850a7ee2e",
+    "url": "/static/css/main.4808bbfa.chunk.css"
+  },
+  {
+    "revision": "6a3d2cecc64035ccf4f8",
+    "url": "/static/js/2.560f3217.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.560f3217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6595b185af850a7ee2e",
+    "url": "/static/js/main.368fe98f.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
